fe0f14e56c60810d99a06ce7d1d7ffc83d54b96f
JojoIV <jonas.broeckmann@gmx.de>
2026-07-16 03:15:31 +0200

    Add utility functions for mapping ZIP entries with and without indices

--- src/commonMain/kotlin/de/jonasbroeckmann/kzip/Extensions.kt
+++ src/commonMain/kotlin/de/jonasbroeckmann/kzip/Extensions.kt
@@ -25,6 +25,26 @@ public inline fun Zip.forEachEntry(crossinline block: (Zip.Entry) -> Unit): Unit
 }
 
 /**
+ * Maps all entries in the ZIP file to a list of results using the given block.
+ *
+ * @param block the block to execute on each index and entry
+ * @return a sequence of results
+ */
+public inline fun <R> Zip.mapEntriesIndexed(crossinline block: (Int, Zip.Entry) -> R): Sequence<R> = (0 until numberOfEntries)
+    .asSequence()
+    .map { i -> entry(i) { block(i, this) } }
+
+/**
+ * Maps all entries in the ZIP file to a list of results using the given block.
+ *
+ * @param block the block to execute on each entry
+ * @return a sequence of results
+ */
+public inline fun <R> Zip.mapEntries(crossinline block: (Zip.Entry) -> R): Sequence<R> = mapEntriesIndexed { _, entry ->
+    block(entry)
+}
+
+/**
  * Deletes entries from the ZIP file.
  *
  * @param paths the paths of the entries to delete
